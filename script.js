abcbeec147ece0c4aa7756f3eb51dcab088a7237
datguyrhy <sirdan1230@gmail.com>
2019-10-01 11:45:25 +0800

    made some minor changes to the UI
    Replaced placeholders for Ketchup,Mayo and Mustard images
    
    Adjusted some margins to make the look more uniform
    
    added a hi-score counter
    
    added some DOM manipulators to adjust how the game looks
    
    adjusted levelUp function for faster progression

--- script.js
+++ script.js
@@ -74,9 +74,10 @@ function pickIngredient(ingredient){
     if (ingredient === 'burgerCap'){
       document.querySelector("#layer" + layerCounter.toString()).style.backgroundColor = "#ec9b3b";
       document.querySelector("#layer" + layerCounter.toString()).style.backgroundColor = "#ec9b3b";
-      document.querySelector("#layer" + layerCounter.toString()).style.borderTopLeftRadius = "10px";
-      document.querySelector("#layer" + layerCounter.toString()).style.borderTopRightRadius = "10px";
+      document.querySelector("#layer" + layerCounter.toString()).style.borderTopLeftRadius = "30px";
+      document.querySelector("#layer" + layerCounter.toString()).style.borderTopRightRadius = "30px";
       document.querySelector("#layer" + layerCounter.toString()).style.width = "380px";
+      document.querySelector("#layer" + layerCounter.toString()).style.height = "60px";
       if(!bun){
         bun = true;
         burgerCheck();
@@ -86,63 +87,78 @@ function pickIngredient(ingredient){
   }
   console.log("layerCounter: " + layerCounter);
   var x = document.querySelector("#layer" + layerCounter.toString())
+
   //ingredient push into playburger
   if (ingredient === 'mushroom'){
     x.style.backgroundImage = 'linear-gradient(#672f2f, #faf5ef)';
+    x.style.width = "360px";
     playburger.push("mushroom");
-  }else if (ingredient === 'cheese'){
+  } else if (ingredient === 'cheese'){
     x.style.backgroundImage = "linear-gradient(#fbe555, #f45905,#fbe555)";
+    x.style.width = "360px";
     playburger.push("cheese");
 
-  }  else if (ingredient === 'beefPatty'){
+  } else if (ingredient === 'beefPatty'){
     x.style.backgroundColor = "#672f2f";
+    x.style.width = "360px";
     playburger.push("beefPatty");
 
-  }  else if (ingredient === 'chixPatty'){
+  } else if (ingredient === 'chixPatty'){
     x.style.backgroundImage = 'linear-gradient(90deg,#eadca6,#c36a2d, #e2c275,#eadca6)';
+    x.style.width = "360px";
     playburger.push("chixPatty");
 
-  }  else if (ingredient === 'fishFillet'){
+  } else if (ingredient === 'fishFillet'){
     x.style.backgroundColor = "#fda77f";
+    x.style.width = "360px";
     playburger.push("fishFillet");
 
-  }  else if (ingredient === 'lettuce'){
+  } else if (ingredient === 'lettuce'){
     x.style.backgroundColor = "#c9d99e";
+    x.style.width = "360px";
     playburger.push("lettuce");
 
-  }  else if (ingredient === 'tomato'){
+  } else if (ingredient === 'tomato'){
     x.style.backgroundColor = "#f66767";
+    x.style.width = "360px";
     playburger.push("tomato");
 
-  }  else if (ingredient === 'bacon'){
+  } else if (ingredient === 'bacon'){
     x.style.backgroundColor = "#b5525c";
+    x.style.width = "360px";
     playburger.push("bacon");
 
   } else if (ingredient === 'pickle'){
     x.style.backgroundColor = "#445c3c";
+    x.style.width = "360px";
     playburger.push("pickle");
 
   } else if (ingredient === 'mayo'){
     x.style.backgroundColor = "#ffffea";
+    x.style.width = "360px";
     playburger.push("mayo");
 
   } else if (ingredient === 'ketchup'){
     x.style.backgroundColor = "#c70d3a";
+    x.style.width = "360px";
     playburger.push("ketchup");
 
   } else if (ingredient === 'mustard'){
     x.style.backgroundColor = "#fbe555";
+    x.style.width = "360px";
     playburger.push("mustard");
 
   } else if (ingredient === 'ham'){
     x.style.backgroundColor = "#fe59d7";
+    x.style.width = "360px";
     playburger.push("ham");
 
   } else if (ingredient === 'burgerCap'){
     x.style.backgroundColor = "#ec9b3b";
-    x.style.borderTopLeftRadius = "10px";
-    x.style.borderTopRightRadius = "10px";
+    x.style.borderTopLeftRadius = "30px";
+    x.style.borderTopRightRadius = "30px";
     x.style.width = "380px";
+    x.style.height = "60px";
     if(!bun){
       bun = true;
       burgerCheck();
@@ -154,6 +170,7 @@ function pickIngredient(ingredient){
 
 var bun = false;
 
+//reset function after cleared
 function burgerCheck(){
   for(var i=0;i< exampleburger.length;i++){
     if(playburger[i] !== exampleburger[i]){
@@ -163,192 +180,207 @@ function burgerCheck(){
     }
   }
   setTimeout(function(){
-  alert("Nice burger!")}, 1000);
-  console.log("where's the lamb sauce");
-  clearedCounter= clearedCounter+1;
-  console.log("cleared: " + clearedCounter);
-  setTimeout(startGame,1200);
-  setTimeout(reset,1100);
-  levelUp();
-};
-//reset function after cleared
-function reset(){
-  playburger.length = 0;
-  exampleburger.length =0;
-  bun = false;
-  for(var i=layerCounter;i>=0;i--){
-    document.querySelector("#layer" + i.toString()).style.backgroundColor ="";
-    document.querySelector("#layer" + i.toString()).style.backgroundImage = "none";
-    document.querySelector("#eglayer" + i.toString()).style.backgroundImage = "none";
-    console.log(i);
+    alert("Nice burger!")}, 1000);
+    console.log("where's the lamb sauce");
+    clearedCounter= clearedCounter+1;
+    console.log("cleared: " + clearedCounter);
+    setTimeout(startGame,1200);
+    setTimeout(reset,1100);
+    setTimeout(score,1100);
+    levelUp();
+
+  };
+  function reset(){
+    playburger.length = 0;
+    exampleburger.length =0;
+    bun = false;
+    for(var i=layerCounter;i>=0;i--){
+      document.querySelector("#layer" + i.toString()).style.backgroundColor ="";
+      document.querySelector("#layer" + i.toString()).style.backgroundImage = "none";
+      document.querySelector("#layer" + i.toString()).style.width = "380px";
+      document.querySelector("#layer" + i.toString()).style.height = "30px";
+      document.querySelector("#layer" + layerCounter.toString()).style.borderTopLeftRadius = "5px";
+      document.querySelector("#layer" + layerCounter.toString()).style.borderTopRightRadius = "5px";
+      document.querySelector("#eglayer" + i.toString()).style.backgroundImage = "none";
+      console.log(i);
+    }
+    layerCounter = 0;
   }
-  layerCounter = 0;
-}
-//startgame generator changes the example burger array
-function startGame(){
-  for(var i= 0; i<maxIngredient;i++){
-    var rng = getRandomInt(0,ingredientList.length);
-    var randomBurgerGen= ingredientList[rng];
-    var x = document.querySelector("#eglayer" + i.toString());
-    if (randomBurgerGen === 'mushroom'){
-      x.style.backgroundImage = "linear-gradient(#672f2f, #faf5ef)";
-      exampleburger.push("mushroom");
-    }else if (randomBurgerGen === 'cheese'){
-      x.style.backgroundImage = "linear-gradient(#fbe555, #f45905,#fbe555)";
-      exampleburger.push("cheese");
+  //startgame generator changes the example burger array
+  function startGame(){
+    for(var i= 0; i<maxIngredient;i++){
+      var rng = getRandomInt(0,ingredientList.length);
+      var randomBurgerGen= ingredientList[rng];
+      var x = document.querySelector("#eglayer" + i.toString());
+      if (randomBurgerGen === 'mushroom'){
+        x.style.backgroundImage = "linear-gradient(#672f2f, #faf5ef)";
+        exampleburger.push("mushroom");
+      }else if (randomBurgerGen === 'cheese'){
+        x.style.backgroundImage = "linear-gradient(#fbe555, #f45905,#fbe555)";
+        exampleburger.push("cheese");
 
-    }  else if (randomBurgerGen === 'beefPatty'){
-      x.style.backgroundColor = "#672f2f";
-      exampleburger.push("beefPatty");
+      }  else if (randomBurgerGen === 'beefPatty'){
+        x.style.backgroundColor = "#672f2f";
+        exampleburger.push("beefPatty");
 
-    }  else if (randomBurgerGen === 'chixPatty'){
-      x.style.backgroundImage = 'linear-gradient(90deg,#eadca6,#c36a2d, #e2c275,#eadca6)';
-      exampleburger.push("chixPatty");
+      }  else if (randomBurgerGen === 'chixPatty'){
+        x.style.backgroundImage = 'linear-gradient(90deg,#eadca6,#c36a2d, #e2c275,#eadca6)';
+        exampleburger.push("chixPatty");
 
-    }  else if (randomBurgerGen === 'fishFillet'){
-      x.style.backgroundColor = "#fda77f";
-      exampleburger.push("fishFillet");
+      }  else if (randomBurgerGen === 'fishFillet'){
+        x.style.backgroundColor = "#fda77f";
+        exampleburger.push("fishFillet");
 
-    }  else if (randomBurgerGen === 'lettuce'){
-      x.style.backgroundColor = "#c9d99e";
-      exampleburger.push("lettuce");
+      }  else if (randomBurgerGen === 'lettuce'){
+        x.style.backgroundColor = "#c9d99e";
+        exampleburger.push("lettuce");
 
-    }  else if (randomBurgerGen === 'tomato'){
-      x.style.backgroundColor = "#f66767";
-      exampleburger.push("tomato");
+      }  else if (randomBurgerGen === 'tomato'){
+        x.style.backgroundColor = "#f66767";
+        exampleburger.push("tomato");
 
-    }  else if (randomBurgerGen === 'bacon'){
-      x.style.backgroundColor = "#b5525c";
-      exampleburger.push("bacon");
+      }  else if (randomBurgerGen === 'bacon'){
+        x.style.backgroundColor = "#b5525c";
+        exampleburger.push("bacon");
 
-    } else if (randomBurgerGen === 'pickle'){
-      x.style.backgroundColor = "#445c3c";
-      exampleburger.push("pickle");
+      } else if (randomBurgerGen === 'pickle'){
+        x.style.backgroundColor = "#445c3c";
+        exampleburger.push("pickle");
 
-    } else if (randomBurgerGen === 'mayo'){
-      x.style.backgroundColor = "#ffffea";
-      exampleburger.push("mayo");
+      } else if (randomBurgerGen === 'mayo'){
+        x.style.backgroundColor = "#ffffea";
+        exampleburger.push("mayo");
 
-    } else if (randomBurgerGen === 'ketchup'){
-      x.style.backgroundColor = "#c70d3a";
-      exampleburger.push("ketchup");
+      } else if (randomBurgerGen === 'ketchup'){
+        x.style.backgroundColor = "#c70d3a";
+        exampleburger.push("ketchup");
 
-    } else if (randomBurgerGen === 'mustard'){
-      x.style.backgroundColor = "#fbe555";
-      exampleburger.push("mustard");
+      } else if (randomBurgerGen === 'mustard'){
+        x.style.backgroundColor = "#fbe555";
+        exampleburger.push("mustard");
 
-    } else if (randomBurgerGen === 'ham'){
-      x.style.backgroundColor = "#fe59d7";
-      exampleburger.push("ham");
+      } else if (randomBurgerGen === 'ham'){
+        x.style.backgroundColor = "#fe59d7";
+        exampleburger.push("ham");
+      }
+    }
+
+    document.querySelector("#eglayer" + maxIngredient.toString()).style.backgroundColor = "#ec9b3b";
+  }
+
+  startGame();
+
+  //copied a RNG function from MDN
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+  //ONLY removes last selected item from the player burgerCap
+  //fixed an issue where burger cap removal pops out ingredients
+  function removeLastLayer(){
+    var x = document.querySelector('#layer'+ layerCounter.toString());
+    if(bun === true){
+      x.style.backgroundColor = "";
+      x.style.backgroundImage = "none";
+      bun = false;
+    }
+    else {
+      x.style.backgroundColor = "";
+      x.style.backgroundImage = "none";
+      playburger.pop();
+    }
+    layerCounter=layerCounter-1;
+    if(layerCounter<0){
+      layerCounter=0;
     }
   }
 
-  document.querySelector("#eglayer" + maxIngredient.toString()).style.backgroundColor = "#ec9b3b";
-}
-
-startGame();
-
-//copied a RNG function from MDN
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-//ONLY removes last selected item from the player burgerCap
-//fixed an issue where burger cap removal pops out ingredients
-function removeLastLayer(){
-  var x = document.querySelector('#layer'+ layerCounter.toString());
-  if(bun === true){
-    x.style.backgroundColor = "";
-    x.style.backgroundImage = "none";
-    bun = false;
+  function removePicked (){
+    for(var i = 0 ; i<11; i++){
+      document.querySelector('#layer'+i.toString()).addEventListener('click', removeLastLayer);
+      console.log("listener added");
+    }
   }
-  else {
-    x.style.backgroundColor = "";
-    x.style.backgroundImage = "none";
-    playburger.pop();
+
+  removePicked();
+
+  function resetBurger(){
+    playburger.length = 0;
+    for(var i=layerCounter;i>=0;i--){
+      document.querySelector("#layer" + i.toString()).style.backgroundColor ="";
+      document.querySelector("#layer" + i.toString()).style.backgroundImage = "none";
+      document.querySelector("#layer" + i.toString()).style.width = "380px";
+      document.querySelector("#layer" + i.toString()).style.height = "30px";
+      document.querySelector("#layer" + i.toString()).style.borderTopLeftRadius = "5px";
+      document.querySelector("#layer" + i.toString()).style.borderTopRightRadius = "5px";
+      layerCounter = 0;
+      bun = false;
+    }
   }
-  layerCounter=layerCounter-1;
-  if(layerCounter<0){
-    layerCounter=0;
+  document.querySelector('#resetBurger').addEventListener('click',resetBurger);
+  //difficulty level Up
+  var clearedCounter=0;
+
+  function levelUp(){
+    if(clearedCounter >1){
+      maxIngredient = 4;
+
+    } if(clearedCounter >2){
+      maxIngredient = 5;
+
+    } if(clearedCounter >3){
+      maxIngredient = 6;
+
+    } if(clearedCounter >4){
+      maxIngredient = 8;
+
+    } if(clearedCounter >5){
+      maxIngredient = 9;
+    }
+    if(clearedCounter >6){
+      maxIngredient = 10;
+    }
   }
-}
 
-function removePicked (){
-  for(var i = 0 ; i<11; i++){
-    document.querySelector('#layer'+i.toString()).addEventListener('click', removeLastLayer);
-    console.log("listener added");
+  function score(){
+    var score =clearedCounter*100;
+    document.querySelector('#Hi-score').innerText =" " + score;
   }
-}
 
-removePicked();
+  //
+  // //weird stuff
+  // var test = { "mushroom" : pickMushroom };
+  // var imgList = { "mushroom" : "mushroom.png" }
+  //
+  // function initButtons()
+  // {
+  //   for(var elem in test)
+  //   {
+  //     var btn = document.querySelector('#' + elem);
+  //     btn.addEventListener('click', test[elem]);
+  //     console.log(btn);
+  //   }
+  // }
+  // initButtons();
+  //
+  // function pickMushroom() {
+  //   ingredientLayer().style.backgroundColor = "brown";
+  //   layerCounter++;
+  // }
+  //
+  // function ingredientLayer()
+  // {     return document.querySelector("#layer" + layerCounter.toString());
+  // }
 
-function resetBurger(){
-  playburger.length = 0;
-  for(var i=layerCounter;i>=0;i--){
-    document.querySelector("#layer" + i.toString()).style.backgroundColor ="";
-    document.querySelector("#layer" + i.toString()).style.backgroundImage = "none";
-    layerCounter = 0;
-    bun = false;
-  }
-}
-document.querySelector('#resetBurger').addEventListener('click',resetBurger);
-//difficulty level Up
-var clearedCounter=0;
-
-function levelUp(){
-  if(clearedCounter >1){
-    maxIngredient = 4;
-
-  } if(clearedCounter >2){
-    maxIngredient = 5;
-
-  } if(clearedCounter >3){
-    maxIngredient = 6;
-
-  } if(clearedCounter >4){
-    maxIngredient = 8;
-
-  } if(clearedCounter >6){
-    maxIngredient = 9;
-  }
-  if(clearedCounter >12){
-    maxIngredient = 10;
-  }
-}
-//
-// //weird stuff
-// var test = { "mushroom" : pickMushroom };
-// var imgList = { "mushroom" : "mushroom.png" }
-//
-// function initButtons()
-// {
-//   for(var elem in test)
-//   {
-//     var btn = document.querySelector('#' + elem);
-//     btn.addEventListener('click', test[elem]);
-//     console.log(btn);
-//   }
-// }
-// initButtons();
-//
-// function pickMushroom() {
-//   ingredientLayer().style.backgroundColor = "brown";
-//   layerCounter++;
-// }
-//
-// function ingredientLayer()
-// {     return document.querySelector("#layer" + layerCounter.toString());
-// }
-
-// function pickIngredient2 (){
-//   var pickCheese = style.backgroundColor = "yellow";
-//   playerburger.push("cheese");
-// }
-// //
-// };
+  // function pickIngredient2 (){
+  //   var pickCheese = style.backgroundColor = "yellow";
+  //   playerburger.push("cheese");
+  // }
+  // //
+  // };
 
 
-//create 3 arrays
-//startbutton to populate array
+  //create 3 arrays
+  //startbutton to populate array
